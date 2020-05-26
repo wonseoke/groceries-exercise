0fdb0e7b73b8e1a5c4df9696c569c5fbda717e56
wonseoke <wonseoke@gmail.com>
2020-05-26 15:08:28 -0400

    Finished Product for Groceries Exercise

--- groceries.py
+++ groceries.py
@@ -36,7 +36,7 @@ products = [
 #import operator
 #products_by_name = sorted(products, key=operator.itemgetter('name'))
 
-'''
+
 print("--------------")
 print("THERE ARE", len(products), "PRODUCTS")
 print("--------------")
@@ -51,7 +51,7 @@ for p in sorted_products:
     #price_usd = p["price"]
     price_usd = " (${0:.2f})".format(p["price"])
     print(" + " + p["name"] + price_usd)
-'''
+
 
 
 departments = []
@@ -69,10 +69,16 @@ print("--------------")
 
 unique_departments.sort()
 
+
 for d in unique_departments:
     matching_products = [p for p in products if p["department"] == d]
     matching_products_count = len(matching_products)
-    print(" + " + d.title() + " (" + str(matching_products_count) + " products)")
+    
+    if matching_products_count > 1:
+        label = "products"
+    else:
+        label = "product"
+    print(" + " + d.title() + " (" + str(matching_products_count) + " " + label + ")")
 
 
 
